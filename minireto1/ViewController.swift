7a9ae1ad51003240ff05cd1d99de7168478be46c
Vicente Álvarez Maestre <vamaestre@gmail.com>
2016-05-06 21:25:32 +0200

    Initial Commit

---
+++ minireto1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  minireto1
+//
+//  Created by Vicente Álvarez Maestre on 6/5/16.
+//  Copyright © 2016 Vicente Álvarez Maestre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
